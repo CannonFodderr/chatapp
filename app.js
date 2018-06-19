4e3ad652611bea7d3a846f6dddde326fce29bab1
CannonFodderr <izhaki.idan@gmail.com>
2018-06-19 13:41:47 +0300

    Add dotenv package with PORT variable

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const   express = require('express'),
         app = express(),
         path = require('path'),
+        dotenv = require('dotenv').config(),
         fs = require('fs'),
         bodyParser = require('body-parser'),
         sanitizer = require('sanitizer'),
-        port = process.env.PORT || 3000,
+        port = process.env.PORT,
         server = app.listen(port, ()=> console.log(`Server is running on port ${port}`)),
         io = require('socket.io')(server);
 
